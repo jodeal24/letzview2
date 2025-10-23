7a5449074a9f2d58e2e0f23d0c211ce56344c9b3
jodeal24 <dealmeida.joao@hotmail.com>
2025-10-23 15:52:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Admin from "./Admin.jsx";
 import "./index.css";
 
+const path = window.location.pathname;
+const Root = path.startsWith("/admin") ? <Admin /> : <App />;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>{Root}</React.StrictMode>
 );
